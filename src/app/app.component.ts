08dcdd1eb6a2791046d8cb2298a52c5ecf01548f
Mandy <mtwmt@hotmail.com>
2022-10-29 19:50:29 +0800

    feat(site): add theme change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2, ViewEncapsulation } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ironman';
+
+  constructor(
+    private renderer: Renderer2,
+    public appService: AppService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.initTheme();
+    console.log(this.document);
+  }
+
+  initTheme(): void {
+    const theme = localStorage.getItem('theme') || '';
+    this.onThemeChange(theme);
+  }
+
+  onThemeChange(theme: string = ''): void {
+    if (!theme) {
+      this.renderer.removeClass(this.document.body, 'dark');
+      localStorage.removeItem('theme');
+    } else {
+      this.renderer.addClass(this.document.body, 'dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+
+  constructor() {}
+}
