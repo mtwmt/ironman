7323ffe8ba5f5c3d7d7af2e6190db399ceda18b3
Mandy <mtwmt@hotmail.com>
2022-10-29 23:32:28 +0800

    refactor(list): refactor filter

--- src/app/ironman/ironman-store.service.ts
+++ src/app/ironman/ironman-store.service.ts
@@ -7,7 +7,6 @@ import {
   Observable,
   of,
   shareReplay,
-  Subject,
   switchMap,
 } from 'rxjs';
 import { IronmanListInfo } from './ironman.model';
@@ -48,95 +47,104 @@ export class IronmanStoreService {
     .fetchHistoryIronman()
     .pipe(shareReplay(1));
 
+
   public getIronmanList$: BehaviorSubject<IronmanListInfo[]> =
     new BehaviorSubject([] as IronmanListInfo[]);
 
   constructor(private ironmanService: IronmanService) {}
+
+  filterYearObservable(year: string = '') {
+    return combineLatest([
+      this.ironman2022$,
+      this.ironman2021$,
+      this.ironman2020$,
+      this.ironman2019$,
+      this.ironman2018$,
+      this.ironman2017$,
+      this.ironman2016$,
+      this.ironman2015$,
+      this.ironman2014$,
+      this.ironmanHistory$,
+    ]).pipe(
+      map(
+        ([
+          i2022,
+          i2021,
+          i2020,
+          i2019,
+          i2018,
+          i2017,
+          i2016,
+          i2015,
+          i2014,
+          iHistory,
+        ]) => {
+          let list: IronmanListInfo[] = [];
+          switch (year) {
+            case '2022':
+              list = i2022;
+              break;
+            case '2021':
+              list = i2021;
+              break;
+            case '2020':
+              list = i2020;
+              break;
+            case '2019':
+              list = i2019;
+              break;
+            case '2018':
+              list = i2018;
+              break;
+            case '2017':
+              list = i2017;
+              break;
+            case '2016':
+              list = i2016;
+              break;
+            case '2015':
+              list = i2015;
+              break;
+            case '2014':
+              list = i2014;
+              break;
+            case 'history':
+              list = iHistory;
+              break;
+            default:
+              list = [
+                ...i2022,
+                ...i2021,
+                ...i2020,
+                ...i2019,
+                ...i2018,
+                ...i2017,
+                ...i2016,
+                ...i2015,
+                ...i2014,
+                ...iHistory,
+              ];
+              break;
+          }
+          return list;
+        }
+      )
+    );
+  }
 
   filterQuery(query: string, year: string = '') {
     of(query)
       .pipe(
         debounceTime(700),
         switchMap((val) => {
-          return combineLatest([
-            this.ironman2022$,
-            this.ironman2021$,
-            this.ironman2020$,
-            this.ironman2019$,
-            this.ironman2018$,
-            this.ironman2017$,
-            this.ironman2016$,
-            this.ironman2015$,
-            this.ironman2014$,
-            this.ironmanHistory$,
-          ]).pipe(
-            map(
-              ([
-                i2022,
-                i2021,
-                i2020,
-                i2019,
-                i2018,
-                i2017,
-                i2016,
-                i2015,
-                i2014,
-                iHistory,
-              ]) => {
-                let list: IronmanListInfo[] = [];
-                switch (year) {
-                  case '2022':
-                    list = i2022;
-                    break;
-                  case '2021':
-                    list = i2021;
-                    break;
-                  case '2020':
-                    list = i2020;
-                    break;
-                  case '2019':
-                    list = i2019;
-                    break;
-                  case '2018':
-                    list = i2018;
-                    break;
-                  case '2017':
-                    list = i2017;
-                    break;
-                  case '2016':
-                    list = i2016;
-                    break;
-                  case '2015':
-                    list = i2015;
-                    break;
-                  case '2014':
-                    list = i2014;
-                    break;
-                  case 'history':
-                    list = iHistory;
-                    break;
-                  default:
-                    list = [
-                      ...i2022,
-                      ...i2021,
-                      ...i2020,
-                      ...i2019,
-                      ...i2018,
-                      ...i2017,
-                      ...i2016,
-                      ...i2015,
-                      ...i2014,
-                      ...iHistory,
-                    ];
-                    break;
-                }
-                return list.filter((d: IronmanListInfo) => {
-                  return d.topic
-                    .toLocaleLowerCase()
-                    .includes(val.toLocaleLowerCase());
-                });
-              }
-            )
+          return this.filterYearObservable(year).pipe(
+            map((list) => {
+              return list.filter((d: IronmanListInfo) => {
+                return d.topic
+                  .toLocaleLowerCase()
+                  .includes(val.toLocaleLowerCase());
+              });
+            })
           );
         })
       )
@@ -144,4 +152,6 @@ export class IronmanStoreService {
         this.getIronmanList$.next(res);
       });
   }
+
+
 }
