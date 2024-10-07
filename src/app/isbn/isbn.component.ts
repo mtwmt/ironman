dceceb3dc0228008cbd3f4c99558fa8b00a04db2
Mandy <mtwmt@hotmail.com>
2024-10-08 01:24:00 +0800

    update

--- src/app/isbn/isbn.component.ts
+++ src/app/isbn/isbn.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BarcodeFormat } from '@zxing/library';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 import { Observable, tap } from 'rxjs';
 
 @Component({
@@ -8,6 +9,8 @@ import { Observable, tap } from 'rxjs';
   templateUrl: './isbn.component.html',
 })
 export class IsbnComponent implements OnInit {
+  @ViewChild(ZXingScannerComponent) scanner!: ZXingScannerComponent;
+
   availableDevices!: MediaDeviceInfo[];
   selectedDevice!: MediaDeviceInfo;
   hasDevices!: boolean;
@@ -15,9 +18,40 @@ export class IsbnComponent implements OnInit {
   formatsEnabled = [BarcodeFormat.EAN_13];
   isbn!: string;
   bookInfo!: any;
+  scanResult: string | null = null;
+
+  mediaConstraints: any = {
+    advanced: [
+      { focusMode: 'continuous' }, // 自動對焦
+      { zoom: true }, // 啟用縮放
+      { torch: false }, // 可選，啟用或停用閃光燈
+    ],
+  };
 
   constructor(private http: HttpClient) {}
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.availableDevices = devices;
+
+      // 預設選擇後鏡頭（如果有）
+      if (devices.length > 0) {
+        this.selectedDevice = devices[0];
+      }
+    });
+
+    this.scanner.camerasNotFound.subscribe(() => {
+      console.error('沒有找到任何相機裝置');
+    });
+
+    this.scanner.permissionResponse.subscribe((granted: boolean) => {
+      if (granted) {
+        console.log('相機權限已授予');
+      } else {
+        console.log('相機權限被拒絕');
+      }
+    });
+  }
 
   onCamerasFound(devices: MediaDeviceInfo[]): void {
     this.availableDevices = devices;
